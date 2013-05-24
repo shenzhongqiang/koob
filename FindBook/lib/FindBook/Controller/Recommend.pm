c1f6744a7cf295550f7c7469e9b0e55a2f850371
Zhongqiang Shen <zqshen.1985@gmail.com>
2013-05-24 08:39:51 +0800

    order by desc

--- FindBook/lib/FindBook/Controller/Recommend.pm
+++ FindBook/lib/FindBook/Controller/Recommend.pm
@@ -71,7 +71,7 @@ sub catalog :Local :Args(1) {
     my $next_page_no = $page_no >= $pages ? 0 : $page_no + 1;
     my $prev_page_url = $c->forward('make_link', [$url, $prev_page_no]);
     my $next_page_url = $c->forward('make_link', [$url, $next_page_no]);
-    my @book_rows = $c->model('FindBookDB::Book')->search({'tag.catalog' => $catalog}, {join => 'tag', page => $page_no, rows => 10});
+    my @book_rows = $c->model('FindBookDB::Book')->search({'tag.catalog' => $catalog}, {join => 'tag', order_by => {-desc => 'id'}, page => $page_no, rows => 10});
     my @books;
     foreach(@book_rows) {
         my $book_hr = $c->forward('/book/list_book_summary', [$_]);
@@ -111,7 +111,7 @@ sub subcat :Local :Args(1) {
     my $catalog = $tag_row->catalog;
     my @catalogs = $c->model('FindBookDB::Tag')->search(undef, {order_by => 'catalog', distinct => 'catalog'})->get_column('catalog')->all;
     my @subcats = $c->model('FindBookDB::Tag')->search({catalog => $catalog}, {order_by => 'id'})->get_column('subcat')->all;
-    my @book_rows = $tag_row->books->search(undef, {page => $page_no, rows => 10});
+    my @book_rows = $tag_row->books->search(undef, {order_by => {-desc => 'id'}, page => $page_no, rows => 10});
     my @books;
     foreach(@book_rows) {
         my $book_hr = $c->forward('/book/list_book_summary', [$_]);
