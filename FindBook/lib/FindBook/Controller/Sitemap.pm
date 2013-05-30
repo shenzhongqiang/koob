f32b7c5e0b3a581f527bc519918961e2c9e39d25
Zhongqiang Shen <zqshen.1985@gmail.com>
2013-05-30 23:12:14 +0800

    generate sitemap

--- FindBook/lib/FindBook/Controller/Sitemap.pm
+++ FindBook/lib/FindBook/Controller/Sitemap.pm
@@ -1,6 +1,7 @@
 package FindBook::Controller::Sitemap;
 use Moose;
 use namespace::autoclean;
+use WWW::Sitemap::XML;
 
 BEGIN {extends 'Catalyst::Controller'; }
 
@@ -24,9 +25,71 @@ Catalyst Controller.
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
 
-    $c->stash(template => "src/sitemap.tt");
+    my $map = WWW::Sitemap::XML->new;
+    my $index_url = $c->uri_for_action("/index");
+    my $rec_url = $c->uri_for_action("/recommend/index");
+    my $date = today();
+    my @book_isbns = $c->model('FindBookDB::Book')->get_column('isbn')->all;
+    my @cats = $c->model('FindBookDB::Tag')->search(undef, {distinct => 'catalog'})->get_column('catalog')->all;
+    my @subcats = $c->model('FindBookDB::Tag')->search(undef, {distinct => 'subcat'})->get_column('subcat')->all;
+    
+    $map->add(
+        loc     => $index_url,
+        lastmod => $date,
+        changefreq => 'weekly',
+        priority => 1.0,
+    );
+    
+    $map->add(
+        loc     => $rec_url,
+        lastmod => $date,
+        changefreq => 'daily',
+        priority => 0.9,
+    );
+    
+    foreach(@cats) {
+        my $cat_url = $c->uri_for_action("/recommend/catalog", $_);
+        $map->add(
+            loc     => $cat_url,
+            lastmod => $date,
+            changefreq => 'daily',
+            priority => 0.8,
+        );
+    }
+   
+    foreach(@subcats) {
+        my $subcat_url = $c->uri_for_action("/recommend/subcat", $_);
+        $map->add(
+            loc     => $subcat_url,
+            lastmod => $date,
+            changefreq => 'daily',
+            priority => 0.7,
+        );
+    }
+   
+    foreach(@book_isbns) {
+        my $book_url = $c->uri_for_action("/book/list", $_);
+        $map->add(
+            loc     => $book_url,
+            lastmod => $date,
+            changefreq => 'weekly',
+            priority => 0.6,
+        );
+    }
+    
+    my $xml = $map->as_xml->toString(1);
+    $c->res->content_type('text/xml');
+    $c->res->body($xml);
 }
 
+
+sub today {
+    my (undef, undef, undef, $mday, $mon, $year) = localtime(time);
+    $year = $year + 1900;
+    $mon = $mon + 1;
+    my $date = sprintf("%d-%02d-%02d", $year, $mon, $mday);
+    return $date;
+}
 
 =head1 AUTHOR
 
