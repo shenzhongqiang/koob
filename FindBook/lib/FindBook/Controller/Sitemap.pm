8c76b3219fc15d2e5ab167419979e5e28b0d06a9
Zhongqiang Shen <zqshen.1985@gmail.com>
2013-06-01 09:33:55 +0800

    sitemap for so

--- FindBook/lib/FindBook/Controller/Sitemap.pm
+++ FindBook/lib/FindBook/Controller/Sitemap.pm
@@ -83,6 +83,66 @@ sub index :Path :Args(0) {
 }
 
 
+sub so :Local :Args(0) {
+    my ( $self, $c ) = @_;
+
+    my $index_url = $c->uri_for_action("/index");
+    my $rec_url = $c->uri_for_action("/recommend/index");
+    my $date = today();
+    my @book_rows = $c->model('FindBookDB::Book')->all;
+    my @cats = $c->model('FindBookDB::Tag')->search(undef, {distinct => 'catalog'})->get_column('catalog')->all;
+    my @subcats = $c->model('FindBookDB::Tag')->search(undef, {distinct => 'subcat'})->get_column('subcat')->all;
+    
+    my @urls;
+    push(@urls, {
+        loc     => $index_url,
+        lastmod => $date,
+        changefreq => 'weekly',
+        priority => 1.0,
+    });
+    
+    push(@urls, {
+        loc     => $rec_url,
+        lastmod => $date,
+        changefreq => 'daily',
+        priority => 0.9,
+    });
+    
+    foreach(@cats) {
+        my $cat_url = $c->uri_for_action("/recommend/catalog", $_);
+        push(@urls, {
+            loc     => $cat_url,
+            lastmod => $date,
+            changefreq => 'daily',
+            priority => 0.8,
+        });
+    }
+   
+    foreach(@subcats) {
+        my $subcat_url = $c->uri_for_action("/recommend/subcat", $_);
+        push(@urls, {
+            loc     => $subcat_url,
+            lastmod => $date,
+            changefreq => 'daily',
+            priority => 0.7,
+        });
+    }
+   
+    foreach(@book_rows) {
+        my $book_url = $c->uri_for_action("/book/list", $_->isbn);
+        push(@urls, {
+            loc     => $book_url,
+            lastmod => $date,
+            changefreq => 'weekly',
+            priority => 0.6,
+        });
+    }
+    
+    $c->res->content_type('text/xml');
+    $c->stash(template => 'src/sitemap_so.tt', urls => \@urls);
+}
+
+
 sub today {
     my (undef, undef, undef, $mday, $mon, $year) = localtime(time);
     $year = $year + 1900;
