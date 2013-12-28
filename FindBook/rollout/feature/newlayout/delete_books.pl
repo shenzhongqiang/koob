4403bfe783a1485616cc25c7be1d5e3772830d3a
Zhongqiang Shen <zqshen.1985@gmail.com>
2013-12-29 02:26:50 +0800

    import new books

--- FindBook/rollout/feature/newlayout/delete_books.pl
+++ FindBook/rollout/feature/newlayout/delete_books.pl
@@ -14,12 +14,13 @@ my $schema = FindBook::Schema->connect(
 my @book_rows = $schema->resultset("Book")->all;
 foreach my $book_row (@book_rows) {
     my $pic = $book_row->pic;
+    my $desc = $book_row->description;
     my $isbn = $book_row->isbn;
     my $dir = dirname(__FILE__);
     my $path = "$dir/../../../root/static/pics/$pic";
     my $sum = `md5sum $path | cut -f1 -d ' '`;
     chomp($sum);
-    if($sum eq "634c5cb7b200c21bff123c9c124d528d") {
+    if($sum eq "634c5cb7b200c21bff123c9c124d528d" || $desc eq "") {
         # delete file
         unlink($path) or die "cannot delete file $path: $!\n";
         # delete rows in DB
