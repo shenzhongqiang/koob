fd7332b9c4c7633f8f2b62f72936dd29e7405b22
Zhongqiang Shen <zqshen.1985@gmail.com>
2013-12-29 01:13:54 +0800

    script to delete books with empty pic

---
+++ FindBook/rollout/feature/newlayout/delete_books.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use FindBin;
+use lib "$FindBin::Bin/../../../lib";
+use FindBook::Schema;
+use File::Basename;
+
+my $schema = FindBook::Schema->connect(
+    'dbi:mysql:findbook', 'findbook', '58862455'
+);
+
+my @book_rows = $schema->resultset("Book")->all;
+foreach my $book_row (@book_rows) {
+    my $pic = $book_row->pic;
+    my $isbn = $book_row->isbn;
+    my $dir = dirname(__FILE__);
+    my $path = "$dir/../../../root/static/pics/$pic";
+    my $sum = `md5sum $path | cut -f1 -d ' '`;
+    chomp($sum);
+    if($sum eq "634c5cb7b200c21bff123c9c124d528d") {
+        # delete file
+        unlink($path) or die "cannot delete file $path: $!\n";
+        # delete rows in DB
+        my $guard = $schema->txn_scope_guard;
+        $book_row->book_tags->delete;
+        $book_row->delete;
+        $guard->commit;
+        print "deleted $isbn\n";
+    }
+}
