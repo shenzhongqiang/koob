8bbcdc9011f004a0b5936efb2b340fe0d63825c5
Zhongqiang Shen <zqshen.1985@gmail.com>
2013-12-29 13:57:36 +0800

    check if image file exists before md5sum check

--- FindBook/rollout/feature/newlayout/delete_books.pl
+++ FindBook/rollout/feature/newlayout/delete_books.pl
@@ -18,18 +18,18 @@ foreach my $book_row (@book_rows) {
     my $isbn = $book_row->isbn;
     my $dir = dirname(__FILE__);
     my $path = "$dir/../../../root/static/pics/$pic";
-    my $sum = `md5sum $path | cut -f1 -d ' '`;
-    chomp($sum);
-    if($sum eq "634c5cb7b200c21bff123c9c124d528d" || $desc eq "") {
-        # delete file
-        if(-e $path) {
-            unlink($path) or warn "cannot delete file $path: $!\n";
+    if(-e $path) {
+        my $sum = `md5sum $path | cut -f1 -d ' '`;
+        chomp($sum);
+        if($sum eq "634c5cb7b200c21bff123c9c124d528d" || $desc eq "") {
+            # delete file
+                unlink($path) or warn "cannot delete file $path: $!\n";
+            # delete rows in DB
+            my $guard = $schema->txn_scope_guard;
+            $book_row->book_tags->delete;
+            $book_row->delete;
+            $guard->commit;
+            print "deleted $isbn\n";
         }
-        # delete rows in DB
-        my $guard = $schema->txn_scope_guard;
-        $book_row->book_tags->delete;
-        $book_row->delete;
-        $guard->commit;
-        print "deleted $isbn\n";
     }
 }
