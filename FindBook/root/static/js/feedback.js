f61fd7d4b7e00d1315d47bea2eecf1fa4a016c9d
Zhongqiang Shen <zqshen.1985@gmail.com>
2014-01-01 10:29:24 +0800

    leave only suggestions in feedback page

--- FindBook/root/static/js/feedback.js
+++ FindBook/root/static/js/feedback.js
@@ -3,16 +3,8 @@ var feedback = {
         var self = this;
         $('#form-fdbk').submit(function(e) {
             e.preventDefault();
-            var index_open_time = $('input[name="index_open_time"]').val();
-            var search_resp_time = $('input[name="search_resp_time"]').val();
-            var search_result_quality = $('input[name="search_result_quality"]').val();
-            var ui_quality = $('input[name="ui_quality"]').val();
             var other_suggestions = $('textarea[name="other_suggestions"]').val();
             var data = {
-                index_open_time: index_open_time,
-                search_resp_time: search_resp_time,
-                search_result_quality: search_result_quality,
-                ui_quality: ui_quality,
                 other_suggestions: other_suggestions
             };
             $.ajax({
